7845eca65b45e7f61df22affd02b641ed47e22fb
maksim.kos <maksim.kos@datatonic.com>
2026-04-16 14:49:50 +0200

    Added dq scans for dimensions in gold layer

---
+++ auto_dq/create_scans.sh
@@ -0,0 +1,29 @@
+export LOCATION="europe-west1"
+export PROJECT_ID="syntio-onboarding-prod"
+export DATASET_ID="mk_gold"
+export SERVICE_ACCOUNT="dbt-mk-user@syntio-onboarding-prod.iam.gserviceaccount.com"
+export DATA_SOURCE_BASE="//bigquery.googleapis.com/projects/$PROJECT_ID/datasets/$DATASET_ID/tables"
+
+# Create the data quality scans
+gcloud dataplex datascans create data-quality products-dq-scan \
+    --location="$LOCATION" \
+    --data-source-resource="$DATA_SOURCE_BASE/dim_products" \
+    --data-quality-spec-file="rules/dq_products.yaml" 
+
+
+gcloud dataplex datascans create data-quality employee-dq-scan \
+    --location="$LOCATION" \
+    --data-source-resource="$DATA_SOURCE_BASE/dim_employee" \
+    --data-quality-spec-file="rules/dq_employee.yaml"
+
+
+gcloud dataplex datascans create data-quality stores-dq-scan \
+    --location="$LOCATION" \
+    --data-source-resource="$DATA_SOURCE_BASE/dim_stores" \
+    --data-quality-spec-file="rules/dq_stores.yaml"
+
+
+# Run the scan 
+gcloud dataplex datascans run products-dq-scan --location="$LOCATION"
+gcloud dataplex datascans run employee-dq-scan --location="$LOCATION" 
+gcloud dataplex datascans run stores-dq-scan --location="$LOCATION" 
