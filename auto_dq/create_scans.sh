8aa2ea18b1ad054fd536b352c7f17375534e9269
maksim.kos <maksim.kos@datatonic.com>
2026-04-17 13:17:36 +0200

    Added workflow and modifications to autodq scans

--- auto_dq/create_scans.sh
+++ auto_dq/create_scans.sh
@@ -4,19 +4,20 @@ export DATASET_ID="mk_gold"
 export DATA_SOURCE_BASE="//bigquery.googleapis.com/projects/$PROJECT_ID/datasets/$DATASET_ID/tables"
 
 # Create the data quality scans
-gcloud dataplex datascans create data-quality products-dq-scan \
+gcloud dataplex datascans create data-quality mk-products-dq-scan \
     --location="$LOCATION" \
     --data-source-resource="$DATA_SOURCE_BASE/dim_products" \
     --data-quality-spec-file="rules/dq_products.yaml" 
 
 
-gcloud dataplex datascans create data-quality employee-dq-scan \
+gcloud dataplex datascans create data-quality mk-employee-dq-scan \
     --location="$LOCATION" \
     --data-source-resource="$DATA_SOURCE_BASE/dim_employee" \
     --data-quality-spec-file="rules/dq_employee.yaml"
+    
 
 
-gcloud dataplex datascans create data-quality stores-dq-scan \
+gcloud dataplex datascans create data-quality mk-stores-dq-scan \
     --location="$LOCATION" \
     --data-source-resource="$DATA_SOURCE_BASE/dim_stores" \
     --data-quality-spec-file="rules/dq_stores.yaml"
--- readme.md
+++ readme.md
@@ -20,3 +20,9 @@ This directory contains a custom FastAPI Python application containerized via Do
 * **`schema.py`**: Contains strict Pydantic models used by the FastAPI app to enforce data quality and validate the schema of incoming US and BR sales payloads.
 * **`config.py`**: Manages environment variables.
 * **`script.sh`**: The deployment script. It provisions the Pub/Sub topics, creates the Service Accounts, builds the Docker image in Artifact Registry, deploys the Cloud Run service, and sets up **Cloud Scheduler** to trigger the consumption endpoints every 5 minutes.
+
+
+### `auto_dq/`
+This directory contains the configuration and automation scripts for Google Cloud Dataplex AutoDQ. It provides automated data quality monitoring for the serving layer (Gold).
+* **`create_scans.sh`**: A shell script that automates the provisioning of Dataplex Data Quality scans.
+* **`rules/`**: A sub-directory containing table-specific YAML definitions for data quality expectations.
